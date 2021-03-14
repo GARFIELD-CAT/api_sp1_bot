f3caf29b5f613d01b7f0a4bac88b88f2f1cce428
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-03-14 22:35:46 +0300

    Удалил лишние переменные. Настроил детальнее логику сообщений о статусе работы.

--- homework.py
+++ homework.py
@@ -22,32 +22,35 @@ logging.basicConfig(
 
 
 def parse_homework_status(homework):
+    """Обрабатывает статус домашней работы."""
     homework_name = homework['homework_name']
-    if homework['status'] != 'approved':
+    homework_status = homework['status']
+    if homework_status == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
+    elif homework_status == 'approved':
         verdict = (
             'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
         )
+    elif homework_status == 'reviewing':
+        return f'Работа {homework_name} взята на проверку ревьюером.'
+    else:
+        verdict = f'Статус работы: {homework["status"]}'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    token = PRAKTIKUM_TOKEN
     # Делаем запрос к API Яндекс.
     homework_statuses = requests.get(
         'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
-        headers={'Authorization': f'OAuth {token}'},
+        headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'},
         params={'from_date': current_timestamp}
     )
     return homework_statuses.json()
 
 
 def send_message(message, bot_client):
-    text = message
-    chat_id = CHAT_ID
     logging.info('Сообщение отправлено.')
-    return bot_client.send_message(chat_id, text)
+    return bot_client.send_message(CHAT_ID, message)
 
 
 def main():
@@ -75,7 +78,7 @@ def main():
             text = f'Бот столкнулся с ошибкой: {e}'
             logging.error(text)
             send_message(text, bot_client)
-            time.sleep(5)
+            time.sleep(60)
 
 
 if __name__ == '__main__':
